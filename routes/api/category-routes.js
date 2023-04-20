4daae5ece532f712a8bddb976e14a20033fe7c62
MattWagner28 <matthewwagner@outlook.com>
2023-04-20 14:52:18 -0400

    seeded database, created model associations, and setup first get categories route

---
+++ routes/api/category-routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+    // be sure to include its associated Products
+  try {
+    const productData = await Category.findAll({
+      include :[{ model: Product }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // be sure to include its associated Products
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const productData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
